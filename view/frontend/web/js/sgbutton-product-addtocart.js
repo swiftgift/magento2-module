e654f5caa81259d2652ea3846d08164ff4fc7061
Alex <redinlens@yandex.ru>
2018-06-17 15:45:45 +0300

    fix sg button disabled

--- view/frontend/web/js/sgbutton-product-addtocart.js
+++ view/frontend/web/js/sgbutton-product-addtocart.js
@@ -10,6 +10,7 @@ define([
         var addtocart_form = elem.parents('#product_addtocart_form');
         var use_swift_gift = false;
         elem.on('click', function(e) {
+            elem.prop('disabled', true);
             use_swift_gift = true;
             addtocart_form.submit();
             e.preventDefault();
@@ -22,6 +23,7 @@ define([
         function handleProductCartAddFormSubmit() {
             uiRegistry.get('localStorage').set('swift_gift_used_init_value', use_swift_gift);
             if (use_swift_gift) {
+                elem.prop('disabled', false);
                 location.href = checkout.checkoutUrl;
             }
         };
